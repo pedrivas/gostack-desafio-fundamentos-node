ecbf60ae7edb914a44fe935dbbbe165da543a74c
pedrivas <42252328+pedrivas@users.noreply.github.com>
2020-04-16 22:10:48 -0300

    resolucao do desafio

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,40 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const outcomeValues = this.transactions.filter(
+      transaction => transaction.type === 'outcome',
+    );
+    const incomeValues = this.transactions.filter(
+      transaction => transaction.type === 'income',
+    );
+    const outcome = outcomeValues.reduce(
+      (prevValue, transaction) => prevValue + transaction.value,
+      0,
+    );
+    const income = incomeValues.reduce(
+      (prevValue, transaction) => prevValue + transaction.value,
+      0,
+    );
+    const total = income - outcome;
+    const balance = {
+      income,
+      outcome,
+      total,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
